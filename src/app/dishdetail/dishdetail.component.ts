acf04e1238364b8a5263ee3289893534757ff36e
Gabushush <shivanisurt19@gmail.com>
2023-01-10 17:54:10 +0530

    Animations Part-1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,15 +3,30 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validator, Validators,NgForm } from '@angular/forms';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Comment } from '../shared/comment';
 
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            transform: 'scale(1.0)',
+            opacity: 1
+        })),
+        state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+        })),
+        transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 
 export class DishdetailComponent implements OnInit {
@@ -27,6 +42,7 @@ export class DishdetailComponent implements OnInit {
   commentForm: FormGroup;
   commentArr:any=[];
   dishcopy: Dish;
+  visibility = 'shown';
 
   @ViewChild('commForm') commentFormDirective:NgForm;
 
@@ -62,11 +78,13 @@ export class DishdetailComponent implements OnInit {
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
 
-    this.route.params.pipe(switchMap((params: Params) =>                  
-        this.dishservice.getDish(params['id']))).subscribe(dish => {    
+    this.route.params.pipe(switchMap((params: Params) => {
+        this.visibility ='hidden'; 
+        return this.dishservice.getDish(params['id']);})).subscribe(dish => {    
           this.dish = dish; 
           this.dishcopy= dish;
           this.setPrevNext(dish.id); 
+          this.visibility = 'shown';
         },
         errMess => this.errMess = <any>errMess);
     console.log("in dish detail", this.dish, this.dishIds)
