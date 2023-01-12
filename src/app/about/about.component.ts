3a944a19ab2302cfc1cfab410e3806de8bd34610
Gabushush <shivanisurt19@gmail.com>
2023-01-13 00:48:22 +0530

    ANIMATIONS Part -2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,11 +3,19 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LeaderService } from '../services/leader.service';
 import { baseURL } from '../shared/baseurl';
+import { flyInOut, expand } from '../animations/app.animation';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  host:{
+    '[@flyInOut]' :'true',
+    'style': 'display: block'
+  },
+  animations:[
+    flyInOut(), expand()
+  ],
 })
 export class AboutComponent implements OnInit {
 
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,8 +3,8 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validator, Validators,NgForm } from '@angular/forms';
-import { trigger, state, transition, style, animate } from '@angular/animations';
 
+import { visibility, flyInOut, expand } from '../animations/app.animation';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Comment } from '../shared/comment';
@@ -14,18 +14,12 @@ import { Comment } from '../shared/comment';
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
+  host:{
+    '[@flyInOut]' :'true',
+    'style': 'display: block'
+  },
   animations: [
-    trigger('visibility', [
-        state('shown', style({
-            transform: 'scale(1.0)',
-            opacity: 1
-        })),
-        state('hidden', style({
-            transform: 'scale(0.5)',
-            opacity: 0
-        })),
-        transition('* => *', animate('0.5s ease-in-out'))
-    ])
+    visibility(),flyInOut(), expand()
   ]
 })
 
